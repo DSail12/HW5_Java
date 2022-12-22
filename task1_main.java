b96299a97e29eae5b0c5b1962a0feae39074bf8a
dsail <dsail@yandex.ru>
2022-12-22 23:23:39 +0300

    commit 1

--- task1_main.java
+++ task1_main.java
@@ -8,13 +8,13 @@ public class task1_main {
     public static void main(String[] args) {
         task1_phonebook myPhoneBook = new task1_phonebook();
 
-        myPhoneBook.addUser("Ivan", "Ivanov");
-        myPhoneBook.addUser("Feofan", "Ignatov");
-        myPhoneBook.addUser("Vasily", "Pupkin");
+        myPhoneBook.addUser("Dmitry", "Semenov");
+        myPhoneBook.addUser("Leonid", "Vasliev");
+        myPhoneBook.addUser("Kristina", "Kozlova");
 
-        myPhoneBook.addPhone("+79991111111", 1);
-        myPhoneBook.addPhone("+79992222222", 2);
-        myPhoneBook.addPhone("+79993333333", 1);
+        myPhoneBook.addPhone("+79289502298", 1);
+        myPhoneBook.addPhone("+79269934556", 2);
+        myPhoneBook.addPhone("+79858458862", 1);
 
         for (ArrayList<String> user : myPhoneBook.getAll()) {
             System.out.println(user);
